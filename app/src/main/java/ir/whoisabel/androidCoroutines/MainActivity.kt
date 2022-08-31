4bb4ce37eae2e3b5f70e71117285de9b5b4fb69c
WhoisAbel <whoisabel1996@gmail.com>
2022-08-31 09:22:42 +0430

    SEQUENTIAL Background Tasks with Kotlin Coroutines (async and await) (#9)

--- app/src/main/java/ir/whoisabel/androidCoroutines/MainActivity.kt
+++ app/src/main/java/ir/whoisabel/androidCoroutines/MainActivity.kt
@@ -1,8 +1,8 @@
 package ir.whoisabel.androidCoroutines
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import ir.whoisabel.androidCoroutines.databinding.ActivityMainBinding
 import kotlinx.coroutines.*
 import kotlinx.coroutines.Dispatchers.IO
@@ -32,7 +32,8 @@ class MainActivity : AppCompatActivity() {
                 //fakeApiResult()
             }
             //fakeApiResultForWorkingWithLaunch()
-            fakeApiResultForWorkingWithAsync()
+            //fakeApiResultForWorkingWithAsync()
+            fakeApiResultForSequential()
         }
 
         binding.btnStartOtherActivity.setOnClickListener {
@@ -102,6 +103,41 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // SEQUENTIAL Background Tasks with Kotlin Coroutines (async and await)
+    private fun fakeApiResultForSequential() {
+        CoroutineScope(IO).launch {
+            val executionTime = measureTimeMillis {
+
+                /*
+                withContext(Dispatchers.Default) {
+                        println("debug: launching job1: ${Thread.currentThread().name}")
+                        getResult1FromApi()
+                    }
+                ===================================
+               async {
+                    println("debug: launching job1: ${Thread.currentThread().name}")
+                    getResult1FromApi()
+                }.await()
+                *
+                *
+                * */
+
+                val result1 =
+                    withContext(Dispatchers.Default) {
+                        println("debug: launching job1: ${Thread.currentThread().name}")
+                        getResult1FromApi()
+                    }
+
+                val result3 = withContext(Dispatchers.Default) {
+                    println("debug: launching job3: ${Thread.currentThread().name}")
+                    println("debug: ${getResult3FromApi(result1)}")
+                }
+
+            }
+            println("debug: total time is $executionTime")
+        }
+    }
+
     private suspend fun fakeApiResult() {
         withContext(IO) {
             val job = withTimeoutOrNull(JOB_TIMEOUT) {
@@ -132,6 +168,15 @@ class MainActivity : AppCompatActivity() {
         logThreadName("getResult1FromApi2")
         delay(1000)
         return RESULT_2
+    }
+
+    private suspend fun getResult3FromApi(result: String): String {
+        logThreadName("getResult1FromApi1")
+        delay(1000)
+        if (result == RESULT_1) {
+            return "SEQUENTIAL Background Tasks with Kotlin Coroutines (async and await)"
+        }
+        throw CancellationException(" Result #1 was incorrect...")
     }
 
     private fun logThreadName(methodName: String) {
